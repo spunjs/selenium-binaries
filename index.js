a33e337128b012e2a2d4e7c1c83cd384853fd929
jsdevel <js.developer.undefined@gmail.com>
2015-02-17 13:35:20 -0700

    Closes jsdevel/webdriver-sync#91:  Cannot find binary paths.

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 'use strict';
 
+var path = require('path');
+var fs = require('fs');
+
 //NOTE: binary-paths.json is created by the install script.
-module.exports = require('./binary-paths.json');
+var binaryPaths = path.resolve(__dirname, 'binary-paths.json');
+
+if (!fs.existsSync(binaryPaths)) {
+  console.error('[selenium-binaries]', 'Could not find', binaryPaths);
+  console.error('[selenium-binaries]', 'This file is generated during the install phase.');
+  console.error('[selenium-binaries]', 'This most likely means some binaries failed to download.');
+  console.error('[selenium-binaries]', 'Please re-install selenium-binaries accordingly.');
+  console.error('[selenium-binaries]', 'If you did not install selenium-binaries directly, it has been added as a dependency somewhere.');
+  console.error('[selenium-binaries]', 'Removing node_modules and running `npm install` may be an appropriate action.');
+  process.exit(1);
+}
+
+module.exports = require(binaryPaths);
