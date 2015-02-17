14923887be571e5d8591bb5e7e85ea8419fffc4e
jsdevel <js.developer.undefined@gmail.com>
2015-02-17 16:00:57 -0700

    Using cli instead of console in index.js.

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 'use strict';
 
+var cli = require('./lib/cli');
 var path = require('path');
 var fs = require('fs');
-
 //NOTE: binary-paths.json is created by the install script.
 var binaryPaths = path.resolve(__dirname, 'binary-paths.json');
 
+var COULD_NOT_FIND_BINARY_PATHS=1
+
 if (!fs.existsSync(binaryPaths)) {
-  console.error('[selenium-binaries]', 'Could not find', binaryPaths);
-  console.error('[selenium-binaries]', 'This file is generated during the install phase.');
-  console.error('[selenium-binaries]', 'This most likely means some binaries failed to download.');
-  console.error('[selenium-binaries]', 'Please re-install selenium-binaries accordingly.');
-  console.error('[selenium-binaries]', 'If you did not install selenium-binaries directly, it has been added as a dependency somewhere.');
-  console.error('[selenium-binaries]', 'Removing node_modules and running `npm install` may be an appropriate action.');
-  process.exit(1);
+  cli.err('Could not find ' + binaryPaths);
+  cli.err('This file is generated during the install phase.');
+  cli.err('This most likely means some binaries failed to download.');
+  cli.err('Please re-install selenium-binaries accordingly.');
+  cli.err('If you did not install selenium-binaries directly, it has been added as a dependency somewhere.');
+  cli.err('Removing node_modules and running `npm install` may be an appropriate action.\n');
+  cli.exit(COULD_NOT_FIND_BINARY_PATHS);
 }
 
 module.exports = require(binaryPaths);
